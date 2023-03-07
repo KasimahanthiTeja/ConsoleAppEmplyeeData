1ff973771d67dbf7247af9766312b3db445daf8a
KasimahanthiTeja <tejakasimahanthi@gmail.com>
2023-03-07 12:36:05 +0530

    Console App Employee Data

---
+++ ConsoleAppemployeedata/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppemployeedata
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int W1 = 101;
+            string I1 = "Abhi";
+            string I2 = "School";
+            decimal I3 = 7658941322m;
+            int X1 = 68;
+            int W2 = 102;
+            string J1 = "Chiru";
+            string J2 = "College";
+            decimal J3 = 8466806514m;
+            int X2 = 63;
+            int W3 = 103;
+            string K1 = "Ravi";
+            string K2 = "School";
+            decimal K3 = 7648327663m;
+            int X3 = 60;
+            int W4 = 104;
+            string L1 = "Vamsi";
+            string L2 = "College";
+            decimal L3 = 8125165478m;
+            int X4 = 64;
+            int W5 = 105;
+            string M1 = "Ganesh";
+            string M2 = "Diploma";
+            decimal M3 = 63033442244m;
+            int X5 = 65;
+            Console.WriteLine("----------------------------------------------------------------------------------------");
+            Console.Write("Roll no        ");
+            Console.Write("Student Name      ");
+            Console.Write("Qualificarion       ");
+            Console.Write("Mobile no         ");
+            Console.WriteLine("Percentage        ");
+            Console.WriteLine("-----------------------------------------------------------------------------------------");
+            Console.Write("W1          ");
+            Console.Write("I1          ");
+            Console.Write("I2          ");
+            Console.Write("I3          ");
+            Console.WriteLine("X1          ");
+
+        }
+    }
+}
